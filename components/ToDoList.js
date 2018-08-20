8c3d5e00a016990c95d91a612b0cc92bb0f4981a
xin hu <xinhu@xins-iMac.fios-router.home>
2018-08-19 22:19:00 -0400

    conditional rendering

--- components/ToDoList.js
+++ components/ToDoList.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Query, withApollo } from "react-apollo";
 import { LIST_TODOS } from "./query";
-import { Text, View, FlatList } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 class ToDoList extends Component {
   render() {
@@ -11,9 +11,27 @@ class ToDoList extends Component {
           if (loading) return <Text>loading</Text>;
 
           return (
-            <View>
-              {data.toDoes.map(post => (
-                <Text>{post.title}</Text>
+            <View style={styles.container}>
+              {data.toDoes.map(toDo => (
+                <View key={toDo.id} style={styles.todoList}>
+                  <View style={styles.todoListItem}>
+                    <View style={styles.acitonSituation}>
+                      <TouchableOpacity>
+                        <View>
+                          {toDo.situation === "In progress" ? (
+                            <View style={styles.inProgress} />
+                          ) : (
+                            <View style={styles.notStarted} />
+                          )}
+                        </View>
+                      </TouchableOpacity>
+                    </View>
+                    <View style={styles.toDo}>
+                      <Text style={styles.title}>{toDo.title}</Text>
+                      <Text style={styles.description}>{toDo.description}</Text>
+                    </View>
+                  </View>
+                </View>
               ))}
             </View>
           );
@@ -22,5 +40,56 @@ class ToDoList extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  todoList: {
+    padding: 10
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#252525"
+  },
+  description: {
+    color: "#9d9d9d",
+    fontSize: 14,
+    marginTop: 8
+  },
+  todoListItem: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 6,
+    flexDirection: "row"
+  },
+  notStarted: {
+    height: 24,
+    width: 24,
+    marginRight: 10,
+    borderStyle: "solid",
+    borderColor: "#9d9d9d",
+    borderRadius: 100,
+    borderWidth: 1.5
+  },
+
+  inProgress: {
+    height: 24,
+    width: 24,
+    marginRight: 10,
+    backgroundColor: "green",
+    borderStyle: "solid",
+    borderColor: "green",
+    borderRadius: 100,
+    borderWidth: 1.5
+  },
+  acitonSituation: {
+    flex: 1
+  },
+  toDo: {
+    flex: 5
+  }
+});
 
 export default withApollo(ToDoList);
