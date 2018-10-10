3d8b021a4779b12a17333de92da0fdd1627df100
mistyhx <hxmisty@gmail.com>
2018-10-10 10:07:05 -0400

    migrated to aws amplify

--- App.js
+++ App.js
@@ -2,17 +2,17 @@ import React from "react";
 import AWSAppSyncClient from "aws-appsync";
 import { ApolloProvider } from "react-apollo";
 import { Rehydrated } from "aws-appsync-react";
-import appSyncConfig from "./AppSync";
+import appSyncConfig from "./aws-exports";
 import Test from "./components/Test";
 
 import Tabnavigator from "./router/Tabnavigator";
 
 const client = new AWSAppSyncClient({
-  url: appSyncConfig.graphqlEndpoint,
-  region: appSyncConfig.region,
+  url: appSyncConfig.aws_appsync_graphqlEndpoint,
+  region: appSyncConfig.aws_appsync_region,
   auth: {
-    type: appSyncConfig.authenticationType,
-    apiKey: appSyncConfig.apiKey
+    type: appSyncConfig.aws_appsync_authenticationType,
+    apiKey: appSyncConfig.aws_appsync_apiKey
   }
 });
 export default class App extends React.Component {
--- App.test.js
+++ App.test.js
@@ -1,7 +1,10 @@
 import React from "react";
 import App from "./App";
-
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
 import renderer from "react-test-renderer";
+
+Amplify.configure(config);
 
 it("renders without crashing", () => {
   const rendered = renderer.create(<App />).toJSON();
